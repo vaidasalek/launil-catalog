37ac25e414ac2fd43282b2c6c7df17681df1fe3a
vaidasalek <57872113+vaidasalek@users.noreply.github.com>
2020-04-17 19:10:21 +0200

    Navigacija

--- root/assets/js/main.js
+++ root/assets/js/main.js
@@ -49,39 +49,47 @@
   });
 
   // Mobile Navigation
-  if ($('.mobile-nav').length) {
-    var $mobile_nav = $('.mobile-nav').clone().prop({
-      class: 'mobile-nav d-lg-none'
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle"><i class="icofont-navigation-menu"></i></button>');
-    $('body').append('<div class="mobile-nav-overly"></div>');
+//  if ($('.mobile-nav').length) {
+//    var $mobile_nav = $('.mobile-nav').clone().prop({
+//      class: 'mobile-nav d-lg-none'
+//    });
+//    $('body').append($mobile_nav);
+//    $('body').prepend('<button type="button" class="mobile-nav-toggle"><i class="icofont-navigation-menu"></i></button>');
+//    $('body').append('<div class="mobile-nav-overly"></div>');
+//
+//    $(document).on('click', '.mobile-nav-toggle', function(e) {
+//      $('body').toggleClass('mobile-nav-active');
+//      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+//      $('.mobile-nav-overly').toggle();
+//    });
+//
+//    $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+//      e.preventDefault();
+//      $(this).next().slideToggle(300);
+//      $(this).parent().toggleClass('active');
+//    });
+//
+//    $(document).click(function(e) {
+//      var container = $(".mobile-nav, .mobile-nav-toggle");
+//      if (!container.is(e.target) && container.has(e.target).length === 0) {
+//        if ($('body').hasClass('mobile-nav-active')) {
+//          $('body').removeClass('mobile-nav-active');
+//          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+//          $('.mobile-nav-overly').fadeOut();
+//        }
+//      }
+//    });
+//  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+//    $(".mobile-nav, .mobile-nav-toggle").hide();
+//  }
 
-    $(document).on('click', '.mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-      $('.mobile-nav-overly').toggle();
-    });
-
-    $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
-      e.preventDefault();
-      $(this).next().slideToggle(300);
-      $(this).parent().toggleClass('active');
-    });
-
-    $(document).click(function(e) {
-      var container = $(".mobile-nav, .mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          $('.mobile-nav-overly').fadeOut();
-        }
-      }
-    });
-  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-    $(".mobile-nav, .mobile-nav-toggle").hide();
-  }
+	$(document).ready(function() {
+// SideNav Button Initialization
+$(".button-collapse").sideNav();
+// SideNav Scrollbar Initialization
+var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+var ps = new PerfectScrollbar(sideNavScrollbar);
+});
 
   // Navigation active state on scroll
   var nav_sections = $('section');
